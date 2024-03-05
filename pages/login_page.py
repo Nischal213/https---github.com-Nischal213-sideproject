d8921f3a46ad10f89a85bf30210f5a154759e82f
Nischal <Letsgomidas@gmail.com>
2024-03-05 22:03:19 +0000

    Added password input with validation

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,11 +8,31 @@ def does_username_exist(username):
     )
 
 
+def is_password_correct(password, username):
+    if does_username_exist(username):
+        with open(f"user_data/{username}.txt") as f:
+            content = f.readlines()[1][10:]
+            if password == content.strip():
+                return True
+    else:
+        return False
+
+
 st.write("Hello another world")
 username = st.text_input("Username", max_chars=20)
 if len(username) == 0:
     st.info("Enter your username")
+    user_result = False
 else:
     user_result = does_username_exist(username)
     if user_result == False:
         st.error("Username does not exist")
+password = st.text_input("Password", type="password")
+if len(password) == 0:
+    st.info(f"Enter a secure password")
+    pass_result = False
+else:
+    pass_result = is_password_correct(password, username)
+    print(pass_result)
+    if pass_result == False:
+        st.error("Invalid password")
