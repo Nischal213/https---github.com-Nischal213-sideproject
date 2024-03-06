3ad61834db2af62cc4e8c75bcfd02e7f48d065be
Nischal <Letsgomidas@gmail.com>
2024-03-06 20:17:16 +0000

    Forced dark mode, updated validation handling with mr.andar's help

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ with open("static/styles.css") as f:
 def is_username_valid(username):
 
     def has_invalid_chars(str):
-        return not (
+        return bool(
             [i for i in str if (i in string.punctuation and i != "_") or i.isspace()]
         )
 
@@ -25,20 +25,23 @@ def is_username_valid(username):
         # a non empty list becomes 1
 
     error_msg = ""
-    if is_username_unique(username) == False:
-        error_msg = "Username already taken"
-        return (False, error_msg)
-    elif len(username) > 20 or len(username) < 3:
-        error_msg = "Username can only be 3-20 chars long"
-        return (False, error_msg)
-    elif has_invalid_chars(username) == False:
-        error_msg = "Username may only contain letters, numbers and _"
-        return (False, error_msg)
-    elif len([i for i in username if i in string.digits]) == len(username):
-        error_msg = "Username may contain private information"
-        return (False, error_msg)
+    if username:
+        if is_username_unique(username) == False:
+            error_msg = "Username already taken"
+            return (False, error_msg)
+        elif len(username) > 20 or len(username) < 3:
+            error_msg = "Username can only be 3-20 chars long"
+            return (False, error_msg)
+        elif has_invalid_chars(username) == True:
+            error_msg = "Username may only contain letters, numbers and _"
+            return (False, error_msg)
+        elif len([i for i in username if i in string.digits]) == len(username):
+            error_msg = "Username may contain private information"
+            return (False, error_msg)
+        else:
+            return (True, error_msg)
     else:
-        return (True, error_msg)
+        return ("", error_msg)
 
 
 def has_special_chars(str):
@@ -54,20 +57,23 @@ def is_password_valid(password):
         return bool([i for i in str if i.islower()])
 
     error_msg = ""
-    if len(password) < 8:
-        error_msg = "Password is must be at least 8 characters long"
-        return (False, error_msg)
-    elif has_special_chars(password) == False:
-        error_msg = "Password must have a special character"
-        return (False, error_msg)
-    elif has_lower_chars(password) == False:
-        error_msg = "Password must have at least 1 lowercase letter"
-        return (False, error_msg)
-    elif has_upper_chars(password) == False:
-        error_msg = "Password must have at least 1 uppercase letter"
-        return (False, error_msg)
+    if password:
+        if len(password) < 8:
+            error_msg = "Password is must be at least 8 characters long"
+            return (False, error_msg)
+        elif has_special_chars(password) == False:
+            error_msg = "Password must have a special character"
+            return (False, error_msg)
+        elif has_lower_chars(password) == False:
+            error_msg = "Password must have at least 1 lowercase letter"
+            return (False, error_msg)
+        elif has_upper_chars(password) == False:
+            error_msg = "Password must have at least 1 uppercase letter"
+            return (False, error_msg)
+        else:
+            return (True, error_msg)
     else:
-        return (True, error_msg)
+        return ("", error_msg)
 
 
 def is_email_valid(email):
@@ -94,41 +100,32 @@ def is_email_valid(email):
         return False
 
     error_msg = ""
-    if does_email_exist(email) == False:
-        error_msg = "Email does not exist"
-        return (False, error_msg)
-    elif is_email_taken(email) == True:
-        error_msg = "Email already in use"
-        return (False, error_msg)
+    if email:
+        if does_email_exist(email) == False:
+            error_msg = "Email does not exist"
+            return (False, error_msg)
+        elif is_email_taken(email) == True:
+            error_msg = "Email already in use"
+            return (False, error_msg)
+        else:
+            return (True, error_msg)
     else:
-        return (True, error_msg)
+        return ("", error_msg)
 
 
 st.write("Hello world")
 username = st.text_input("Username", max_chars=20)
-if len(username) == 0:
-    st.info(f"Enter a unique username")
-    user_result = False
-else:
-    user_result, user_error = is_username_valid(username)
-    if user_result == False:
-        st.error(f"{user_error}")
+user_result, user_error = is_username_valid(username)
+if user_result == False:
+    st.error(f"{user_error}")
 password = st.text_input("Password", type="password")
-if len(password) == 0:
-    st.info(f"Enter a secure password")
-    pass_result = False
-else:
-    pass_result, pass_error = is_password_valid(password)
-    if pass_result == False:
-        st.error(f"{pass_error}")
+pass_result, pass_error = is_password_valid(password)
+if pass_result == False:
+    st.error(f"{pass_error}")
 email = st.text_input("Email", max_chars=100)
-if len(email) == 0:
-    st.info(f"Enter your email")
-    email_result = False
-else:
-    email_result, email_error = is_email_valid(email)
-    if email_result == False:
-        st.error(f"{email_error}")
+email_result, email_error = is_email_valid(email)
+if email_result == False:
+    st.error(f"{email_error}")
 submit_button = st.button("Sign Up", type="secondary")
 st.markdown(
     "<p> Already have an account? click <a href='/login_page' target='_self'>Here</a></p>",
--- pages/login_page.py
+++ pages/login_page.py
@@ -6,37 +6,35 @@ with open("static/styles.css") as f:
 
 
 def does_username_exist(username):
-    return bool(
-        [i for i in os.listdir("user_data") if i.removesuffix(".txt") == username]
-    )
+    if username:
+        return bool(
+            [i for i in os.listdir("user_data") if i.removesuffix(".txt") == username]
+        )
+    else:
+        return ""
 
 
 def is_password_correct(password, username):
-    if does_username_exist(username):
-        with open(f"user_data/{username}.txt") as f:
-            content = f.readlines()[1][10:]
-            if password == content.strip():
-                return True
+    if username:
+        if does_username_exist(username):
+            with open(f"user_data/{username}.txt") as f:
+                content = f.readlines()[1][10:]
+                if password == content.strip():
+                    return True
+        else:
+            return False
     else:
-        return False
+        return ""
 
 
 st.write("Hello another world")
 username = st.text_input("Username", max_chars=20)
-if len(username) == 0:
-    st.info("Enter your username")
-    user_result = False
-else:
-    user_result = does_username_exist(username)
-    if user_result == False:
-        st.error("Username does not exist")
+user_result = does_username_exist(username)
+if user_result == False:
+    st.error("Username does not exist")
 password = st.text_input("Password", type="password")
-if len(password) == 0:
-    st.info(f"Enter a secure password")
-    pass_result = False
-else:
+if user_result:
     pass_result = is_password_correct(password, username)
-    print(pass_result)
     if pass_result == False:
         st.error("Invalid password")
 submit_button = st.button("Log In", type="secondary")
