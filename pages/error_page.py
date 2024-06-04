c92c2b634ec38f899ebfa5baccb2909097667e9a
Nischal <Letsgomidas@gmail.com>
2024-06-04 22:26:03 +0100

    Working on adding a 2FA for when logging in also updated .toml file to hide the sidebar

--- pages/error_page.py
+++ pages/error_page.py
@@ -13,3 +13,38 @@ st.write(
     "<div id = 'header_error'><h4>Click <a href='http://localhost:8501/login_page' target='_self'> here </a> to be redirected to the main page </h4></div>",
     unsafe_allow_html=True,
 )
+
+import random
+import ssl
+import smtplib
+from email.message import EmailMessage
+
+email_sender = "mathsmaestro123@gmail.com"
+email_password = "mnwg homw fgap fika"
+email_receiver = "krishnakumra0116@yahoo.com"
+random_code = "".join([str(random.randint(0, 9)) for i in range(6)])
+
+
+subject = "Maths Maestro Verification Code"
+body = f"""
+Hello,
+
+This is your 6-digit verification code:
+{random_code}
+Make sure to not let anyone else know this code
+
+Sincerely,
+Maths Maestro
+"""
+em = EmailMessage()
+em["From"] = email_sender
+em["To"] = email_receiver
+em["Subject"] = subject
+em.set_content(body)
+
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
