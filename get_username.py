bfcedc0ae4fd35127c5fa289193da5d54c64fe54
Nischal213 <146193244+Nischal213@users.noreply.github.com>
2024-07-18 13:38:47 +0100

    Create get_username.py

---
+++ get_username.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+from streamlit_extras.switch_page_button import switch_page
+
+if "user" in st.session_state:
+    switch_page("error page")
+
+
+def does_username_exist(username):
+    if username:
+        return any(i == username for i in df["Username"])
+    else:
+        return ""
+
+
+st.header("Enter your username")
+df = pd.read_csv("main_data/data.csv")
+username = st.text_input(
+    "Username", max_chars=20, placeholder="Enter a unique username"
+)
+submit = st.button("Submit", type="primary")
+if submit:
+    if does_username_exist(username):
+        st.session_state["temp_user"] = username
+        switch_page("forgot page")
+    else:
+        st.error("Username does not exist")
